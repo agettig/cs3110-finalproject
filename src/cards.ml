5597ae2ed18b07c27dec701a7edb2ce5a27692ff
Your Name <your_netid@cornell.edu>
2021-09-24 18:12:16 -0400

    eshita tile changes

---
+++ src/cards.ml
@@ -0,0 +1,13 @@
+(*These are all the career options in our game*)
+let police_officer = {name: "Police Officer"; salary: 40000; salary_max: 70000; taxes_due: 15000; college: false}
+let doctor = {name: "Doctor"; salary: 100000; salary_max: 1000000; taxes_due: 45000; college: true}
+let lawyer = {name: "Lawyer"; salary: 90000; salary_max: 1000000; taxes_due: 40000; college: true}
+let mechanic = {name: "Mechanic"; salary: 30000; salary_max: 60000; taxes_due: 10000; college: false}
+let accountant = {name: "Accountant"; salary: 70000; salary_max: 110000; taxes_due: 30000; college: true}
+let computer_designer = {name: "Computer Designer"; salary: 50000; salary_max: 80000; taxes_due: 20000; college: true}
+let hair_stylist = {name: "Hair Stylist"; salary: 30000; salary_max: 60000; taxes_due: 10000; college: false}
+let teacher = {name: "Teacher"; salary: 40000; salary_max: 70000; taxes_due: 15000; college: true}
+let athlete = {name: "Athlete"; salary: 60000; salary_max: 1000000; taxes_due: 25000; college: false}
+let veterinarian = {name: "Veterinarian"; salary: 100000; salary_max: 120000; taxes_due: 35000; college: true}
+let entertainer = {name: "Entertainer"; salary: 50000; salary_max: 1000000; taxes_due: 20000; college: false}
+let salesperson = {name: "Salesperson"; salary: 20000; salary_max: 50000; taxes_due: 5000; college: false}
--- src/types.ml
+++ src/types.ml
@@ -1,11 +1,12 @@
 type cards = 
 |House of {name: string; price:int; selling_price:int}
-|Career of {name: string; salary: int; salary_max: int; taxes_due: int}
+|Career of {name: string; salary: int; salary_max: int; taxes_due: int; college: bool}
 |Share_the_Wealth
 |Long_Term_Investment of int
 |Life_Tiles of int
 
 type player = {name: string; children: int; so: bool; deck: cards list; 
+<<<<<<< HEAD
 account_balance: int; debt:int; pay_raise:int }
 
 type tiles =
@@ -17,3 +18,6 @@ type tiles =
 | HouseTile
 
 
+=======
+account_balance: int; debt:int; pay_raise:int; college: bool}
+>>>>>>> ff0e0411805f6ea68b1246f19d712eb99e52f08c
