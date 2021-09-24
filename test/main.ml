585cc97e5db87468a820f8d87e7cf25ea2f47f26
Connor Chen <cjc353@cornell.edu>
2021-09-24 17:06:31 -0400

    created bank operation add_money but no test yet

--- test/main.ml
+++ test/main.ml
@@ -1,2 +1,10 @@
-(* test test *)
+open OUnit2
+
+let tests = "test suite for sum" >::: [
+  "empty" >:: (fun _ -> assert_equal 0 0);
+  "singleton" >:: (fun _ -> assert_equal 1 1));
+]
+
 let () = print_endline "Hello, world! - test file"
+
+let _ = run_test_tt_main tests
