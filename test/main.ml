327567e6d9d34605af89b61946cfb0c98964586d
Connor Chen <cjc353@cornell.edu>
2021-10-31 21:43:12 -0400

    fixed most tests

--- test/main.ml
+++ test/main.ml
@@ -109,6 +109,14 @@ let test_gamestate =
     deck = [];
   }
 
+let test_gamestate_no_stops =
+  {
+    current_player = test_player_stop_3;
+    players = [ test_player_stop_3; test_player_stop_4 ];
+    tiles = [];
+    deck = [];
+  }
+
 let test_gamestate_finished =
   {
     current_player = test_player_finished;
@@ -124,36 +132,45 @@ let tests =
   "test suite for sum"
   >::: [
          ( "Type player intialization" >:: fun _ ->
-           assert_equal 0 test_player.account_balance );
+           assert_equal 10000 test_player.account_balance );
          ( "Bank operation add_balance" >:: fun _ ->
-           assert_equal 100 test_player_add.account_balance );
+           assert_equal 10100 test_player_add.account_balance );
          ( "Bank operation payraise" >:: fun _ ->
            assert_equal 10000 test_player_payraise.pay_raise );
          ( "Bank operation add_balance negative" >:: fun _ ->
-           assert_equal 15000 test_player_debt.account_balance );
+           assert_equal 5000 test_player_debt.account_balance
+             ~printer:string_of_int );
          ( "Bank operation calculate_loans balance" >:: fun _ ->
-           assert_equal 15000
-             (Bank.calculate_loans test_player_debt).account_balance );
+           assert_equal 5000
+             (Bank.calculate_loans test_player_debt).account_balance
+             ~printer:string_of_int );
          ( "Bank operation calculate_loans debt" >:: fun _ ->
-           assert_equal 50000
-             (Bank.calculate_loans test_player_debt).debt );
+           assert_equal 25000
+             (Bank.calculate_loans test_player_debt).debt
+             ~printer:string_of_int );
          ( "Bank operation pay_college" >:: fun _ ->
            assert_equal 100000 test_player_college.debt );
          ( "Bank operation payday no payraise" >:: fun _ ->
-           assert_equal 40000 test_player_payday.account_balance );
+           assert_equal 50000 test_player_payday.account_balance
+             ~printer:string_of_int );
          ( "Bank operation payday payraise" >:: fun _ ->
-           assert_equal 50000
-             test_player_payday_raise_no_cap.account_balance );
+           assert_equal 60000
+             test_player_payday_raise_no_cap.account_balance
+             ~printer:string_of_int );
          ( "Bank operation payday salary max cap" >:: fun _ ->
-           assert_equal 70000 test_player_payday_cap.account_balance );
+           assert_equal 80000 test_player_payday_cap.account_balance
+             ~printer:string_of_int );
          ( "Bank operation tax no debt" >:: fun _ ->
-           assert_equal 25000 test_player_tax.account_balance );
+           assert_equal 35000 test_player_tax.account_balance
+             ~printer:string_of_int );
          ( "Bank operation tax debt check balance" >:: fun _ ->
-           assert_equal 5000 test_player_tax_debt.account_balance );
+           assert_equal 15000 test_player_tax_debt.account_balance
+             ~printer:string_of_int );
          ( "Bank operation tax debt check debt" >:: fun _ ->
            assert_equal 25000 test_player_tax_debt.debt );
          ( "Bank operation final balance" >:: fun _ ->
-           assert_equal 410000 test_player_final_balance );
+           assert_equal 420000 test_player_final_balance
+             ~printer:string_of_int );
          ( "Player operation add_children" >:: fun _ ->
            assert_equal 2 test_player_children.children );
          ( "Player operation add_significant_other" >:: fun _ ->
@@ -169,28 +186,33 @@ let tests =
          );
          ( "Gamestate operation change_index_board" >:: fun _ ->
            assert_equal false
-             (test_player_index_change.index_on_board
+             ((fst test_player_index_change).index_on_board
             = test_player.index_on_board) );
          ( "Gamestate operation change_index_board stop college"
          >:: fun _ ->
            assert_equal 10
-             (change_index_board test_player_stop_1).index_on_board );
+             (fst (change_index_board test_player_stop_1))
+               .index_on_board );
          ( "Gamestate operation change_index_board stop marriage"
          >:: fun _ ->
            assert_equal 25
-             (change_index_board test_player_stop_2).index_on_board );
+             (fst (change_index_board test_player_stop_2))
+               .index_on_board );
          ( "Gamestate operation change_index_board stop starter home"
          >:: fun _ ->
            assert_equal 33
-             (change_index_board test_player_stop_3).index_on_board );
+             (fst (change_index_board test_player_stop_3))
+               .index_on_board );
          ( "Gamestate operation change_index_board stop house"
          >:: fun _ ->
            assert_equal 97
-             (change_index_board test_player_stop_4).index_on_board );
+             (fst (change_index_board test_player_stop_4))
+               .index_on_board );
          ( "Gamestate operation change_index_board stop retire"
          >:: fun _ ->
            assert_equal 130
-             (change_index_board test_player_stop_5).index_on_board );
+             (fst (change_index_board test_player_stop_5))
+               .index_on_board );
          ( "Gamestate operation current_player" >:: fun _ ->
            assert_equal test_player (current_player test_gamestate) );
          ( "Gamestate operation finished false" >:: fun _ ->
@@ -198,8 +220,9 @@ let tests =
          ( "Gamestate operation finished true" >:: fun _ ->
            assert_equal true (finished test_player_finished) );
          ( "Gamestate operation next_player" >:: fun _ ->
-           assert_equal test_player_add
-             (next_player test_player test_gamestate.players) );
+           assert_equal test_player_stop_4
+             (next_player test_gamestate_no_stops.current_player
+                test_gamestate_no_stops.players) );
          ( "Gamestate operation next_player wrap" >:: fun _ ->
            assert_equal test_player
              (next_player test_player_add test_gamestate.players) );
