5a7ec5cda92947f6d9334c1765469f21e92538c2
Ariela Gettig <aqg3@cornell.edu>
2021-11-28 20:44:35 -0500

    documentation of gamestate

--- src/gamestate.ml
+++ src/gamestate.ml
@@ -36,6 +36,7 @@ let rec index_in_list_helper player lst c =
       if h.name = player.name then c
       else index_in_list_helper player t (c + 1)
 
+(* indexes of stop tiles *)
 let ( married_index,
       starter_home_index,
       house_index,
@@ -62,12 +63,18 @@ let index_in_list_next (current_player : player) (lst : player list) :
   in
   (index_in_list_helper current_player lst 0 + x) mod List.length lst
 
+(** [next_player current_player players] returns player whose turn is
+    after [current_player] in [players] *)
 let next_player current_player players =
   List.nth players (index_in_list_next current_player players)
 
+(** [finished player] returns true if the index of [player] is greater
+    than or equal to the final tile index*)
 let finished player = player.index_on_board >= final_tile_index
 
-let get_tile index tiles : tiles = List.nth tiles index
+(** [get_tile i tiles] returns the tile with index [i]. Raises Failure
+    if [tiles] is shorter than [i]. Raises Invalid Argument if [i] < 0 *)
+let get_tile i tiles : tiles = List.nth tiles i
 
 (** [has_investment numSpun cards] returns true if [numSpun] matches a
     long term investment card within [cards], else false*)
@@ -337,6 +344,8 @@ let rec lawsuit_player players plaintiff =
           lawsuit_player players plaintiff
       | Some x -> x)
 
+(** [change_index_board player] returns a tuple of [player] with a new
+    index and the number they spun *)
 let change_index_board (player : player) : player * int =
   let current_index = player.index_on_board in
   let spinner = spinner () in
@@ -385,8 +394,9 @@ let change_index_board (player : player) : player * int =
   print_endline "------------------------------------------";
   ({ player with index_on_board = new_index }, spinner)
 
-(** [new_players_lst] returns an updated player with the current players
-    record updated*)
+(** [new_players_lst] returns an updated players list by swapping
+    [updated_player] players with players of the same name in
+    [current_lst] to update all players after a turn *)
 let rec new_players_list
     (current_lst : player list)
     (updated_player : player list) =
@@ -399,11 +409,15 @@ let rec new_players_list
            current_lst)
         t
 
+(** [gameover players] returns true if all players have reached the
+    final tile else returns false *)
 let rec gameover players =
   match players with
   | [] -> true
   | h :: t -> if not (finished h) then false else gameover t
 
+(** [player_winner players_lst player] returns the player with the
+    highest final balance *)
 let rec player_winner player_lst player =
   match player_lst with
   | [] -> player
@@ -411,8 +425,12 @@ let rec player_winner player_lst player =
       if final_balance h > final_balance player then player_winner t h
       else player_winner t player
 
-(** [winner player] prints the name of [player] as the winner*)
-let winner player = Printf.printf "Winner %s!\n\n" player.name
+(** [winner player] prints the name of [player] as the winner and their
+    final balance *)
+let winner player =
+  Printf.printf
+    "%s has won The Game of Life with a final balance of %i!\n \n"
+    player.name (final_balance player)
 
 (** [has_career deck] returns Some career card if there is a career card
     in [deck], else None*)
@@ -515,14 +533,103 @@ let rec get_players lst =
         " | " ^ String.sub h.name 0 3 ^ " | " )
       :: get_players t
 
+(** [payraise current_player player_moved player_index] increases the
+    pay raise of the [player_moved] by $10,000 if a payraise tile is
+    passed *)
+let payraise current_player player_moved player_index =
+  if current_player.index_on_board < 39 && 39 <= player_index then
+    payraise player_moved
+  else if current_player.index_on_board < 71 && 71 <= player_index then
+    payraise player_moved
+  else if current_player.index_on_board < 99 && 99 <= player_index then
+    payraise player_moved
+  else if current_player.index_on_board < 114 && 114 <= player_index
+  then payraise player_moved
+  else player_moved
+
+(** [pay current_player payraise_player player_index] increases the
+    account balance of [payraise_player] by their salary + payraises if
+    a pay tile is passed *)
+let pay gamestate payraise_player player_index =
+  if gamestate.current_player.index_on_board < 12 && 12 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 15 && 15 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 23 && 23 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 32 && 32 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 48 && 48 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 57 && 57 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 64 && 64 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 79 && 79 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 86 && 86 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 92 && 92 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 105 && 105 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 109 && 109 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 120 && 120 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else if
+    gamestate.current_player.index_on_board < 127 && 127 <= player_index
+  then
+    let () = print_iter print_payday 0 14 gamestate.graphics in
+    payday payraise_player
+  else payraise_player
+
 let rec turn gamestate : unit =
   print_endline "";
   Printf.printf "%s's Turn \n \nPlease enter any key to start: "
     gamestate.current_player.name;
   (* makes current player type in anything into terminal to start their
-     turn*)
+     turn *)
   start_turn ();
-  (*print current player information*)
+  (* print current player information *)
   player_to_string gamestate.current_player;
   if gameover gamestate.players then
     winner
@@ -535,126 +642,27 @@ let rec turn gamestate : unit =
           next_player gamestate.current_player gamestate.players;
       }
   else
-    (*player with new index*)
     let change_ind_tup = change_index_board gamestate.current_player in
+    (*player with new index*)
     let player_moved = fst change_ind_tup in
+    (* value of spinner *)
     let numSpun = snd change_ind_tup in
     let player_index = player_moved.index_on_board in
-
+    (* prints wedding graphic if index of player is marriage tile*)
     let () =
       if player_index = married_index then
         print_iter print_wedding 0 14 gamestate.graphics
     in
-
+    (* player with update payraise if a payraise tile is passed *)
     let payraise_player =
-      if
-        gamestate.current_player.index_on_board < 39
-        && 39 <= player_index
-      then payraise player_moved
-      else if
-        gamestate.current_player.index_on_board < 71
-        && 71 <= player_index
-      then payraise player_moved
-      else if
-        gamestate.current_player.index_on_board < 99
-        && 99 <= player_index
-      then payraise player_moved
-      else if
-        gamestate.current_player.index_on_board < 114
-        && 114 <= player_index
-      then payraise player_moved
-      else player_moved
+      payraise gamestate.current_player player_moved player_index
     in
 
-    let pay_player =
-      if
-        gamestate.current_player.index_on_board < 12
-        && 12 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 15
-        && 15 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 23
-        && 23 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 32
-        && 32 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 48
-        && 48 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 57
-        && 57 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 64
-        && 64 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 79
-        && 79 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 86
-        && 86 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 92
-        && 92 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 105
-        && 105 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 109
-        && 109 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 120
-        && 120 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else if
-        gamestate.current_player.index_on_board < 127
-        && 127 <= player_index
-      then
-        let () = print_iter print_payday 0 14 gamestate.graphics in
-        payday payraise_player
-      else payraise_player
-    in
+    (* player with updated account balance if a payraise tile is
+       passed *)
+    let pay_player = pay gamestate payraise_player player_index in
 
-    (*tile on which [paid_player] is on*)
+    (* tile on which [paid_player] is on *)
     let tile = get_tile pay_player.index_on_board gamestate.tiles in
     print_tiles tile;
 
