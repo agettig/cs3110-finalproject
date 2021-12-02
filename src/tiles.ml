c56872a214dd1acc0f8f1b3aa32e8459b4e2b87c
Your Name <eshitasangani@gmail.com>
2021-12-02 09:50:25 -0500

    eshita fix tiles list

--- src/tiles.ml
+++ src/tiles.ml
@@ -52,20 +52,33 @@ let extract_pay_tile json =
   let index_tile = json |> member "index_tile" |> to_int in
   PayTile { name; account_change; index_tile }
 
+let extract_pay_tile_list json =
+  json |> member "pay tile" |> to_list |> List.map extract_pay_tile
+
 let extract_life_tile json =
   let name = json |> member "name" |> to_string in
   let index_tile = json |> member "index_tile" |> to_int in
   LifeTile { name; index_tile }
+
+let extract_life_tile_list json =
+  json |> member "life tile" |> to_list |> List.map extract_life_tile
 
 let extract_career_tile json =
   let name = json |> member "name" |> to_string in
   let index_tile = json |> member "index_tile" |> to_int in
   CareerTile { name; index_tile }
 
+let extract_career_tile_list json =
+  json |> member "career tile" |> to_list
+  |> List.map extract_career_tile
+
 let extract_take_tile json =
   let name = json |> member "name" |> to_string in
   let index_tile = json |> member "index_tile" |> to_int in
   TakeTile { name; index_tile }
+
+let extract_take_tile_list json =
+  json |> member "take tile" |> to_list |> List.map extract_take_tile
 
 let extract_family_tile json =
   let name = json |> member "name" |> to_string in
@@ -74,20 +87,35 @@ let extract_family_tile json =
   let children = json |> member "children" |> to_int in
   FamilyTile { name; account_change; index_tile; children }
 
+let extract_family_tile_list json =
+  json |> member "family tile" |> to_list
+  |> List.map extract_family_tile
+
 let extract_house_tile json =
   let name = json |> member "name" |> to_string in
   let index_tile = json |> member "index_tile" |> to_int in
   HouseTile { name; index_tile }
+
+let extract_house_tile_list json =
+  json |> member "house tile" |> to_list |> List.map extract_house_tile
 
 let extract_lawsuit_tile json =
   let name = json |> member "name" |> to_string in
   let index_tile = json |> member "index_tile" |> to_int in
   LawsuitTile { name; index_tile }
 
+let extract_lawsuit_tile_list json =
+  json |> member "lawsuit tile" |> to_list
+  |> List.map extract_lawsuit_tile
+
 let extract_spin_to_win_tile json =
   let name = json |> member "name" |> to_string in
   let index_tile = json |> member "index_tile" |> to_int in
   SpinToWinTile { name; index_tile }
+
+let extract_spin_to_win_tile_list json =
+  json |> member "spintowin tile" |> to_list
+  |> List.map extract_spin_to_win_tile
 
 let extract_action_tile json =
   let name = json |> member "name" |> to_string in
@@ -95,10 +123,44 @@ let extract_action_tile json =
   let position_change = json |> member "position_change" |> to_int in
   ActionTile { name; index_tile; position_change }
 
+let extract_action_tile_list json =
+  json |> member "action tile" |> to_list
+  |> List.map extract_action_tile
+
 let extract_taxes_tile json =
   let name = json |> member "name" |> to_string in
   let index_tile = json |> member "index_tile" |> to_int in
   TaxesTile { name; index_tile }
+
+let extract_taxes_tile_list json =
+  json |> member "taxes tile" |> to_list |> List.map extract_taxes_tile
+
+let tiles = Yojson.Basic.from_file "data/tiles.json"
+
+let pay_tile = extract_pay_tile_list tiles
+
+let life_tile = extract_life_tile_list tiles
+
+let career_tile = extract_career_tile_list tiles
+
+let take_tile = extract_take_tile_list tiles
+
+let family_tile = extract_family_tile_list tiles
+
+let house_tile = extract_house_tile_list tiles
+
+let lawsuit_tile = extract_lawsuit_tile_list tiles
+
+let spintowin_tile = extract_spin_to_win_tile_list tiles
+
+let action_tile = extract_action_tile_list tiles
+
+let taxes_tile = extract_taxes_tile_list tiles
+
+let gold_tiles =
+  pay_tile @ life_tile @ career_tile @ take_tile @ family_tile
+  @ house_tile @ lawsuit_tile @ spintowin_tile @ action_tile
+  @ taxes_tile
 
 let print_tiles = function
   | PayTile x ->
